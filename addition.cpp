03cc43e7d8e0a115c043ae0dd80dade31b235a28
Medha Gupta <medhagupta312002@gmail.com>
2020-06-02 16:36:37 +0530

    updated

---
+++ addition.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main()
+{
+	int a,b,c;
+
+	cout<<"enter two numbers : ";
+
+	cin>> a , b;
+
+	c=a+b;
+
+	cout<<c;
+	cout<<"done";
+
+	return 0;
+}
